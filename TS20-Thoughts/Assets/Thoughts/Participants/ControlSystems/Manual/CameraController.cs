20204d1564ec45dbb6de7a826cc378208bffedc6
guplem <guplem@gmail.com>
2021-03-13 18:07:45 +0100

    Horizontal rotation working

--- TS20-Thoughts/Assets/Thoughts/Participants/ControlSystems/Manual/CameraController.cs
+++ TS20-Thoughts/Assets/Thoughts/Participants/ControlSystems/Manual/CameraController.cs
@@ -9,37 +9,43 @@ namespace Thoughts.ControlSystems
     {
 
         [SerializeField] private new Camera camera;
-        [SerializeField] private GameObject cameraRig;
+        [SerializeField] private Transform cameraRig;
         private Manual manualControlSystem;
 
-        [SerializeField] private float moveSpeed = 1f;
+        [SerializeField] private float moveSpeed = 0.1f;
+        [SerializeField] private float rotationSpeed = 2f;
         [SerializeField] private float fastSpeedMultiplier = 2f;
-        [SerializeField] private float translationTime = 5f;
-
-        [SerializeField] private float rotationSpeed = 1f;
+        [SerializeField] private float movementSmoothing = 5f;
 
         private Vector3 newPosition;
+        private Quaternion newRotation;
 
         private void Awake()
         {
             manualControlSystem = this.GetComponentRequired<Thoughts.ControlSystems.Manual>();
-            newPosition = cameraRig.transform.position;
+            newPosition = cameraRig.position;
+            newRotation = cameraRig.rotation;
         }
 
         private void Update()
         {
             //camera.gameObject.transform.LookAt(cameraRig.transform);
-            HandleTranslation();
+            HandleTransformUpdates();
         }
         
-        private void HandleTranslation()
+        private void HandleTransformUpdates()
         {
-            cameraRig.transform.position = Vector3.Lerp(cameraRig.transform.position, newPosition, Time.deltaTime * translationTime);
+            cameraRig.position = Vector3.Lerp(cameraRig.position, newPosition, Time.deltaTime * movementSmoothing);
+            cameraRig.rotation = Quaternion.Lerp(cameraRig.rotation, newRotation, Time.deltaTime * movementSmoothing);
         }
         
         public void Move(Vector3 desiredTranslation, bool isFastSpeed)
         {
-            newPosition += desiredTranslation * (moveSpeed * (isFastSpeed? fastSpeedMultiplier : 1) );
+            newPosition += cameraRig.rotation * (desiredTranslation * (moveSpeed * (isFastSpeed? fastSpeedMultiplier : 1) ) );
+        }
+        public void Rotate(float desiredRotation, bool isFastSpeed)
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * (desiredRotation * (rotationSpeed * (isFastSpeed? fastSpeedMultiplier : 1) ) ) ) ;
         }
     }
 }
--- TS20-Thoughts/Assets/Thoughts/Participants/ControlSystems/Manual/InputHandler.cs
+++ TS20-Thoughts/Assets/Thoughts/Participants/ControlSystems/Manual/InputHandler.cs
@@ -17,13 +17,17 @@ namespace Thoughts.ControlSystems
 
         private void Update()
         {
-            CameraInput();
+            handleCameraInput();
         }
-        private void CameraInput()
+        private void handleCameraInput()
         {
+            bool isFastSpeed = Input.GetButton("Shift");
+            
             float verticalTranslation = 0f;
             Vector3 desiredTranslation = new Vector3(Input.GetAxis("Horizontal"), verticalTranslation, Input.GetAxis("Vertical")).normalized;
-            manualControlSystem.cameraController.Move(desiredTranslation, Input.GetButton("Shift"));
+            manualControlSystem.cameraController.Move(desiredTranslation, isFastSpeed);
+
+            manualControlSystem.cameraController.Rotate(Input.GetAxis("Rotation"), isFastSpeed);
         }
     }
 }
