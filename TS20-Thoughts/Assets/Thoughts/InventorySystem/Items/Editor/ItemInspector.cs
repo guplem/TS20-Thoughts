6ebc3413ad4d98e9c01e9cd5a36f0179ced42a4d
guplem <guplem@gmail.com>
2021-03-24 18:15:27 +0100

    Item inspector working

--- TS20-Thoughts/Assets/Thoughts/InventorySystem/Items/Editor/ItemInspector.cs
+++ TS20-Thoughts/Assets/Thoughts/InventorySystem/Items/Editor/ItemInspector.cs
@@ -12,46 +12,63 @@ namespace Thoughts
     [CustomEditor(typeof(Item))]
     public class ItemInspector : UnityEditor.Editor
     {
-        private Type[] implementations;
-        private int selectedImplementationIndex = -1;
         private Item item;
+        private Type[] actionsImplementations;
+        private int selectedActionImplementationIndex = -1;
+        private Type[] needsImplementations;
+        private int[] selectedCoveredNeedImplementationIndex;
 
-        private int[] selectedNeedsImplementationIndex;
-        
-        
         public override void OnInspectorGUI()
         {
-            
             // Update the serializedProperty - always do this in the beginning of OnInspectorGUI.
             serializedObject.Update ();
             
-            //specify type
+            //specify target type
             item = target as Item;
             if (item == null) { return; }
-            
-            //find all implementations of IMobAction using System.Reflection.Module
-            if (implementations == null)
-                implementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<IMobAction>();
-            
-            if (selectedNeedsImplementationIndex == null)
-                UpdateSelectedNeedsImplementationIndex();
+
+            if (selectedCoveredNeedImplementationIndex == null)
+                UpdateSelectedCoveredNeedImplementationIndex();
 
             EditorGUILayout.Space();
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Item's actions", EditorStyles.boldLabel);
-            GUILayout.Label(" ");
-            //select an implementation from all found using an editor popup
-            selectedImplementationIndex = EditorGUILayout.Popup(new GUIContent(""),
-                selectedImplementationIndex, implementations.Select(impl => impl.Name).ToArray());
+            
+                GUILayout.Label("Item's actions", EditorStyles.boldLabel);
+                GUILayout.Label(" ");
+                //select an implementation from all found using an editor popup
+                NewActionsSection();
+                
+            EditorGUILayout.EndHorizontal();
+            
+            CheckActionsListConfiguration();
 
+            ShowActionsArray();
+
+            EditorGUILayout.Space();  
+            EditorGUILayout.Space(); 
+            // Draw horizontal line
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider); 
+            
+            ImplementationsSearchSection();
+
+            // Apply changes to the serializedProperty - always do this in the end of OnInspectorGUI.
+            serializedObject.ApplyModifiedProperties ();
+        }
+        private void NewActionsSection()
+        {
+            //find all implementations of IMobAction using System.Reflection.Module
+            if (actionsImplementations == null)
+                actionsImplementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<IMobAction>();
+
+            selectedActionImplementationIndex = EditorGUILayout.Popup(new GUIContent(""),
+                selectedActionImplementationIndex, actionsImplementations.Select(impl => impl.Name).ToArray());
             
             IMobAction newAction = null;
             if (GUILayout.Button("Add action"))
             {
                 //Create a new action of the selected type
-                newAction = (IMobAction) Activator.CreateInstance(implementations[selectedImplementationIndex]);
+                newAction = (IMobAction) Activator.CreateInstance(actionsImplementations[selectedActionImplementationIndex]);
             }
-            EditorGUILayout.EndHorizontal();
 
             //If a new action has been created...
             if (newAction != null)
@@ -62,95 +79,79 @@ namespace Thoughts
                 if (item.actions == null)
                     item.actions = new List<IMobAction>();
                 item.actions.Add(newAction);
-                UpdateSelectedNeedsImplementationIndex();
+                UpdateSelectedCoveredNeedImplementationIndex();
             }
-
-            //// Draw horizontal line
-            //EditorGUILayout.Space(); EditorGUILayout.LabelField("", GUI.skin.horizontalSlider); EditorGUILayout.Space();
-
+        }
+        
+        private void CheckActionsListConfiguration()
+        {
             if (item.actions != null)
             {
                 for (int a = 0; a < item.actions.Count; a++)
                 {
                     if (item.actions[a] == null)
                         EditorGUILayout.HelpBox("The action with index " + a + " is null.\nRecommended to delete the array element by right clicking on it.", MessageType.Warning);
-                
+
                     if (item.actions.Count() != item.actions.Distinct().Count())
                     {
-                        for (int d = a+1; d < item.actions.Count; d++)
+                        for (int d = a + 1; d < item.actions.Count; d++)
                         {
-                            if (item.actions[a] != null && (item.actions[a] == item.actions[d]) )
+                            if (item.actions[a] != null && (item.actions[a] == item.actions[d]))
                                 EditorGUILayout.HelpBox("The actions with index " + a + " and " + d + " are the same object.", MessageType.Warning);
                         }
                     }
                 }
             }
+        }
         
-            //EditorGUI.indentLevel += 1;
-            //EditorGUILayout.Space(); 
-            
-            ShowActionsArray(serializedObject.FindProperty("actions"));
-
-            //EditorGUI.indentLevel -= 1;
-            
-            // Draw horizontal line
-            EditorGUILayout.Space(); EditorGUILayout.Space();  
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider); 
-            
-            // Implementations search
+        private void ImplementationsSearchSection()
+        {
+            // ACTIONS
             EditorGUILayout.BeginHorizontal();
-            if (implementations != null) EditorGUILayout.LabelField($"Found {implementations.Count()} implementations", EditorStyles.helpBox);
-            if (implementations == null || GUILayout.Button("Search implementations"))
-            {
-                //find all implementations of IAction using System.Reflection.Module
-                implementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<IMobAction>();
-            }
+            if (actionsImplementations != null) EditorGUILayout.LabelField($"Found {actionsImplementations.Count()} actions implementations", EditorStyles.helpBox);
+            if (actionsImplementations == null || GUILayout.Button("Search actions implementations"))
+                actionsImplementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<IMobAction>();
             EditorGUILayout.EndHorizontal();
             
-            
-            
-            
-            // Apply changes to the serializedProperty - always do this in the end of OnInspectorGUI.
-            serializedObject.ApplyModifiedProperties ();
+            // NEEDS
+            EditorGUILayout.BeginHorizontal();
+            if (needsImplementations != null) EditorGUILayout.LabelField($"Found {needsImplementations.Count()} needs implementations", EditorStyles.helpBox);
+            if (needsImplementations == null || GUILayout.Button(" Search needs implementations "))
+                needsImplementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<INeed>();
+            EditorGUILayout.EndHorizontal();
         }
-        private void UpdateSelectedNeedsImplementationIndex()
+        
+        private void UpdateSelectedCoveredNeedImplementationIndex()
         {
-            Debug.Log("Updating selectedNeedsImplementationIndex");
-            selectedNeedsImplementationIndex = new int[item.actions.Count];
-            for (int i = 0; i < selectedNeedsImplementationIndex.Length; i++)
-                selectedNeedsImplementationIndex[i] = -1;
+            selectedCoveredNeedImplementationIndex = new int[item.actions.Count];
+            for (int i = 0; i < selectedCoveredNeedImplementationIndex.Length; i++)
+                selectedCoveredNeedImplementationIndex[i] = -1;
         }
 
-
-
-
-
-        private void ShowActionsArray(UnityEditor.SerializedProperty list)
+        private void ShowActionsArray()
         {
+            UnityEditor.SerializedProperty actionsList = serializedObject.FindProperty("actions");
+            
             UnityEditor.EditorGUI.indentLevel += 1;
-            for (int actionIndex = 0; actionIndex < list.arraySize; actionIndex++)
+            for (int actionIndex = 0; actionIndex < actionsList.arraySize; actionIndex++)
             {
                 EditorGUILayout.Space();
+                
                 using (new GUILayout.VerticalScope(EditorStyles.helpBox))
                 {
-                    SerializedProperty actionProperty = list.GetArrayElementAtIndex(actionIndex);
+                    SerializedProperty actionProperty = actionsList.GetArrayElementAtIndex(actionIndex);
 
                     MobAction action = ((MobAction) item.actions[actionIndex]);
                     
+                    // Action name
                     string itemName;
-                    if (action.GetActionName().IsNullOrEmpty())
-                        itemName = $"Action [{actionIndex}]";
-                    else
-                        itemName = $"'{action.GetActionName()}' action [{actionIndex}]";
-                    
+                    if (action.GetActionName().IsNullOrEmpty()) itemName = $"Action [{actionIndex}] ({action.GetType().Name})";
+                    else itemName = $"'{action.GetActionName()}' action [{actionIndex}] ({action.GetType().Name})";
+
                     EditorGUILayout.PropertyField(actionProperty, new GUIContent(itemName), true);
-                    //EditorGUILayout.LabelField(action.GetType().Name);
+
+                    CoveredNeedsSection(action, actionIndex, actionProperty);
                     
-                    //var childrenProperties = transformProp.GetVisibleChildren();
-                    //foreach (var property in childrenProperties) {
-                    //    Debug.Log($"Action {i}: {property.name}");
-                    //}
-                    DisplayActionEditor(action, actionIndex, actionProperty);
                     EditorGUILayout.Space();
                 }
                 
@@ -160,18 +161,12 @@ namespace Thoughts
             UnityEditor.EditorGUI.indentLevel -= 1;
         }
         
-        
-        
-        
-        
-        private void DisplayActionEditor(MobAction action, int actionIndex, SerializedProperty actionProperty)
+        private void CoveredNeedsSection(MobAction action, int actionIndex, SerializedProperty actionProperty)
         {
+            // START
             EditorGUI.indentLevel += 1;
 
-            
-            
-            
-            // COVERED NEEDS
+            // CURRENT COVERED NEEDS
             EditorGUILayout.Separator();
             EditorGUILayout.LabelField("Covered Needs: ", EditorStyles.boldLabel);
             SerializedProperty needsCoveredList = actionProperty.FindPropertyRelative("needsCovered");
@@ -187,42 +182,32 @@ namespace Thoughts
                 SerializedProperty satisfactionProperty = needProperty.FindPropertyRelative("satisfactionAmount");
                 GUILayout.Label("Satisfaction:");
                 satisfactionProperty.intValue = EditorGUILayout.IntField(satisfactionProperty.intValue);
-                GUILayout.Label(" ");
+                GUILayout.Label("  ");
 
                 EditorGUILayout.EndHorizontal();
             }
 
-            /*foreach (NeedSatisfaction coveredNeed in action.needsCovered)
-            {
-                GUILayout.Label("NEED");
-            }*/
-            
-            
-            
-            
-            
-            
             //ADD NEED ELEMENTS
             EditorGUILayout.Space();
-            Type[] needsImplementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<INeed>();     
+            if (needsImplementations == null)
+                needsImplementations = Essentials.Utils.GetTypeImplementationsNotUnityObject<INeed>();     
             
             EditorGUILayout.BeginHorizontal();
             
-                GUILayout.Label(" ");
-                selectedNeedsImplementationIndex[actionIndex] = EditorGUILayout.Popup(new GUIContent(""), selectedNeedsImplementationIndex[actionIndex], needsImplementations.Select(impl => impl.Name).ToArray());
-               
-                Need newNeed = null;
+                GUILayout.Label("    ");
+                
+                selectedCoveredNeedImplementationIndex[actionIndex] = EditorGUILayout.Popup(new GUIContent(""), selectedCoveredNeedImplementationIndex[actionIndex], needsImplementations.Select(impl => impl.Name).ToArray());
+                
                 if (GUILayout.Button("Add covered need"))
                 {
-                    newNeed = (Need) Activator.CreateInstance(needsImplementations[selectedNeedsImplementationIndex[actionIndex]]);
+                    Need newNeed = (Need) Activator.CreateInstance(needsImplementations[selectedCoveredNeedImplementationIndex[actionIndex]]);
                     action.needsCovered.Add(new NeedSatisfaction(newNeed));
-                    action.needsCovered.DebugLog();
                 }
                 
             EditorGUILayout.EndHorizontal();
-            
-            EditorGUI.indentLevel -= 1;
 
+            // END
+            EditorGUI.indentLevel -= 1;
         }
     }
 }
