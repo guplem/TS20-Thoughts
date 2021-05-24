3134abdcf76ca9c0218a393b32ca4fa3c920e110
Guillem Poy <guplem@gmail.com>
2021-05-24 19:53:54 +0200

    Documentation of AppManager

--- TS20-Thoughts/Assets/Thoughts/AppManager.cs
+++ TS20-Thoughts/Assets/Thoughts/AppManager.cs
@@ -3,23 +3,47 @@ using UnityEngine;
 
 namespace Thoughts
 {
+    /// <summary>
+    /// Object that must be kept always alive while running the app. It initializes the game process and keeps track of the current "GameManager".
+    /// </summary>
     public class AppManager : MonoBehaviour
     {
+        /// <summary>
+        /// The singleton instance of this class (AppManager)
+        /// </summary>
         public static AppManager instance { get; private set; }
+        
+        /// <summary>
+        /// The GameObject prefab that is going to be spawned to initialize a new game process.
+        /// </summary>
         [SerializeField] private GameObject gameManagerPrefab;
+        
+        /// <summary>
+        /// The current GameManager of the game/app
+        /// </summary>
         public static GameManager gameManager { get; private set; }
 
+        /// <summary>
+        /// Tries to set up this "AppManager" as singleton instance. If possible, marks the GameObject containing this MonoBehaviour as "DontDestroyOnLoad".
+        /// </summary>
         private void Awake()
         {
             if (InitializeSingleton())
                 DontDestroyOnLoad(gameObject);
         }
         
+        /// <summary>
+        /// Triggers the start of a new game.
+        /// </summary>
         void Start()
         {
             StartNewGame();
         }
         
+        /// <summary>
+        /// If there is no other instance of an "AppManager", this is set as it. Being the only one following the "singleton" pattern.
+        /// </summary>
+        /// <returns>True if it became the singleton instance, false if another "AppManager" set up as the singleton instance already exists.</returns>
         private bool InitializeSingleton()
         {
 
@@ -33,8 +57,12 @@ namespace Thoughts
             return false;
         }
         
+        /// <summary>
+        /// Starts a new game by instantiating the GameManager prefab and triggering its method "StartNewGame"
+        /// </summary>
         private void StartNewGame()
         {
+            //To-Do: Destroy the previous GameManager (that contains all the data/info/state of the previous game) 
             gameManager = Instantiate(gameManagerPrefab).GetComponentRequired<GameManager>();
             gameManager.StartNewGame();
         }
