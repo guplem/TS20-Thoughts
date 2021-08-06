1f888a24de6865ddd28332f815b0671e6b4bdb8a
Guillem Poy <guplem@gmail.com>
2021-08-06 19:59:38 +0200

    Multiple colors shader graph

--- TS20-Thoughts/Assets/Thoughts/Game/Map/Generation/Terrain/TextureData.cs
+++ TS20-Thoughts/Assets/Thoughts/Game/Map/Generation/Terrain/TextureData.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TextureData", menuName = "Thoughts/Map/Terrain/TextureData", order = 22)]
 public class TextureData : UpdatableData
 {
-
+    [SerializeField] public Material material;
+    
+    [Space]
     public Color color0;
-    public Color color1;
+    [Space]
     [Range(0, 1)]
     public float color1Start;
+    public Color color1;
+    [Space]
+    [Range(0, 1)]
+    public float color2Start;
+    public Color color2;
 
     private float minHeight;
     private float maxHeight;
     
-    [SerializeField] public Material material;
-    
     public void ApplyToMaterial()
     {
         material.SetColor("color0", color0);
-        material.SetColor("color1", color1);
         material.SetFloat("color1Start", color1Start);
+        material.SetColor("color1", color1);
+        material.SetFloat("color2Start", color2Start);
+        material.SetColor("color2", color2);
         
         UpdateMeshHeights(minHeight, maxHeight);
     }
