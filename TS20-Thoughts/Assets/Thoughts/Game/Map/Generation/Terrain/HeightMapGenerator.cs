a94dd4f3b0273a3aa2e7fddc64af38624d565891
Guillem Poy <guplem@gmail.com>
2021-08-17 18:28:58 +0200

    Re implemented falloff

--- TS20-Thoughts/Assets/Thoughts/Game/Map/Generation/Terrain/HeightMapGenerator.cs
+++ TS20-Thoughts/Assets/Thoughts/Game/Map/Generation/Terrain/HeightMapGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public static class HeightMapGenerator
 {
+    static float[,] falloffMap;
+    
     public static HeightMap GenerateHeightMap(int width, int height, HeightMapSettings settings, Vector2 sampleCenter)
     {
         float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCenter);
@@ -13,11 +15,17 @@ public static class HeightMapGenerator
         float minValue = float.MaxValue;
         float maxValue = float.MinValue;
         
+        if (settings.useFalloff) {
+            if (falloffMap == null) {
+                falloffMap = FalloffGenerator.GenerateFalloffMap (width);
+            }
+        }
+        
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                values[i, j] *= heigtCurve_threadSafe.Evaluate(values[i, j]) * settings.heightMultiplier;
+                values [i, j] *= heigtCurve_threadSafe.Evaluate (values [i, j] - (settings.useFalloff ? falloffMap[i, j] : 0)) * settings.heightMultiplier;
 
                 if (values[i, j] > maxValue)
                     maxValue = values[i, j];
