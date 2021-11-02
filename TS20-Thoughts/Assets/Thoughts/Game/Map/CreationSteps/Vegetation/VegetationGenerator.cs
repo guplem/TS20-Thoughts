1bcb50240f1d4ca6e39d2ff56cd85ba7bda79c26
Guillem Poy <guplem@gmail.com>
2021-11-02 20:09:12 +0100

    Update VegetationGenerator.cs

--- TS20-Thoughts/Assets/Thoughts/Game/Map/CreationSteps/Vegetation/VegetationGenerator.cs
+++ TS20-Thoughts/Assets/Thoughts/Game/Map/CreationSteps/Vegetation/VegetationGenerator.cs
@@ -17,7 +17,6 @@ namespace Thoughts.Game.Map
         private int vegetationSeed => _randomNumberToAlterMainSeed + mapGenerator.mapConfiguration.seed; //IT MUST NEVER CHANGE
         private const int _randomNumberToAlterMainSeed = 5151335; //IT MUST NEVER CHANGE and be completely unique per generator (except the mapGenerator and those that do not need randomness)
         
-        
         private void GenerateVegetation(bool clearPrevious)
         {
             if (clearPrevious)
