4252a2d0568b5e3eddfefa1f0966d78c38c41cd5
Guillem Poy <guplem@gmail.com>
2021-11-10 18:33:04 +0100

    value of properties in UI

--- TS20-Thoughts/Assets/Thoughts/Participants/ControlSystems/Manual/UI/Game/Shared/PropertyUI.cs
+++ TS20-Thoughts/Assets/Thoughts/Participants/ControlSystems/Manual/UI/Game/Shared/PropertyUI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using Thoughts.Game.Map.MapElements.Properties;
 using TMPro;
 using UnityEngine;
@@ -14,7 +16,18 @@ namespace Thoughts.Participants.ControlSystems.Manual.UI.Game.Shared
         /// A reference to the text component that displays the name of the displayed Property
         /// </summary>
         [Tooltip("A reference to the text component that displays the name of the displayed Property")]
-        [SerializeField] private TMP_Text propertyText;
+        [SerializeField] private TMP_Text propertyName;
+        
+        /// <summary>
+        /// A reference to the text component that displays the value of the displayed Property
+        /// </summary>
+        [Tooltip("A reference to the text component that displays the value of the displayed Property")]
+        [SerializeField] private TMP_Text propertyValue;
+        
+        /// <summary>
+        /// The displayed property ownership
+        /// </summary>
+        private PropertyOwnership propertyOwnership;
 
         /// <summary>
         /// Displays the given Property in the UI. Disables the GameObject if the Property is null.
@@ -26,8 +39,17 @@ namespace Thoughts.Participants.ControlSystems.Manual.UI.Game.Shared
 
             gameObject.SetActive(display);
 
+            this.propertyOwnership = propertyOwnership;
+
             if (display)
-                propertyText.text = propertyOwnership.property.name;
+            {
+                propertyName.text = propertyOwnership.property.name;
+            }
+        }
+
+        private void Update()
+        {
+            propertyValue.text = this.propertyOwnership.value.ToString(CultureInfo.InvariantCulture);
         }
 
     }
