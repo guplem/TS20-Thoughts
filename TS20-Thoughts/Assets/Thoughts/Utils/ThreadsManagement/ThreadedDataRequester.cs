5d43add0f8894551209d8d300104d8309958c73b
guplem <guplem@gmail.com>
2021-11-24 09:51:46 +0100

    Cleanup of unnecessary comments

--- TS20-Thoughts/Assets/Thoughts/Utils/ThreadsManagement/ThreadedDataRequester.cs
+++ TS20-Thoughts/Assets/Thoughts/Utils/ThreadsManagement/ThreadedDataRequester.cs
@@ -11,16 +11,6 @@ namespace Thoughts.Utils.ThreadsManagement
     [ExecuteAlways]
     public class ThreadedDataRequester : MonoBehaviour
     {
-        /*private static ThreadedDataRequester instance { get => _instance; set => _instance = value; }
-    private static ThreadedDataRequester _instance;*/
-    
-        /*private void Awake()
-    {
-        if (instance != null)
-            Debug.LogWarning($"More than 1 ThreadedDataRequester objects exist: '{instance.ToString()}' and '{this.ToString()}'", this);
-        else
-            instance = this;
-    }*/
     
         /// <summary>
         /// Queue containing data obtained in threads waiting to be shared with the callbacks that were given during the request
@@ -32,47 +22,25 @@ namespace Thoughts.Utils.ThreadsManagement
         /// </summary>
         /// <param name="generateDataMethod">The method containing the logic to generate the data in a thread</param>
         /// <param name="callback">The method to call once the computation has been completed</param>
-        public void RequestData(Func<object> generateDataMethod, Action<object> callback/*, MapConfiguration mapConfiguration*/)
+        public void RequestData(Func<object> generateDataMethod, Action<object> callback)
         {
-            /*if (instance == null)
-            Debug.LogWarning($"The static instance (singleton) for {nameof(ThreadedDataRequester)} is null.");*/
-        
             ThreadStart threadStart = delegate
             {
-                DataThread(generateDataMethod, callback/*, mapConfiguration*/);
+                DataThread(generateDataMethod, callback);
             };
             new Thread(threadStart).Start();
         }
     
-        private void DataThread(Func<object> generateDataMethod, Action<object> callback/*, MapConfiguration mapConfiguration*/)
+        private void DataThread(Func<object> generateDataMethod, Action<object> callback)
         {
             object data = generateDataMethod();
-            //HeightMap heightMapAbsolute = HeightMapGenerator.GenerateHeightMap( mapConfiguration.chunkSize, mapConfiguration.chunkSize, mapConfiguration.terrainData.heightMapSettings, centre);
+            
             lock (dataQueue)
             {
                 dataQueue.Enqueue(new ThreadInfo(callback, data));
             }
         }
-    
-        /*public void RequestMeshData(HeightMap heightMapAbsolute, MapConfiguration mapConfiguration, int LOD, Action<MeshData> callback)
-    {
-        ThreadStart threadStart = delegate
-        {
-            HeightMapThread(heightMapAbsolute, mapConfiguration, LOD, callback);
-        };
-        
-        new Thread(threadStart).Start();
-    }*/
-    
-        /*public void HeightMapThread(HeightMap heightMapAbsolute, MapConfiguration mapConfiguration, int LOD, Action<MeshData> callback)
-    {
-        MeshData meshData = MapDisplay.GenerateTerrainMesh(heightMapAbsolute.values, mapConfiguration, LOD);
-        lock (terrainDataThreadInfoQueue)
-        {
-            meshDataThreadInfoQueue.Enqueue(new ThreadInfo<MeshData>(callback, meshData));
-        }
-    }*/
-    
+
         private void Update()
         {
             if (dataQueue.Count > 0)
@@ -83,14 +51,7 @@ namespace Thoughts.Utils.ThreadsManagement
                     threadInfo.callback(threadInfo.data);
                 }
             }
-            /*if (meshDataThreadInfoQueue.Count > 0)
-        {
-            for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
-            {
-                ThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
-                threadInfo.callback(threadInfo.parameter);
-            }
-        }*/
+
         }
     }
 }
